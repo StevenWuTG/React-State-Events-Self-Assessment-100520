ccc7590bf35b8f53987eadd53332dd9061f371e2
StevenWuTG <stevenwu91@icloud.com>
2020-12-09 22:31:00 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
+import ImageContainer from './Container/ImageContainer';
 function App() {
-  return <h1>You Can Do This!</h1>;
+  return <ImageContainer />;
 }
 
 export default App;
